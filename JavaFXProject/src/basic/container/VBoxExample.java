2bfbfeafcc91c9e6ce0f7f6efba1458e00b571fe
seonae <seonae@kakao.com>
2020-09-01 17:53:29 +0900

    .

--- JavaFXProject/src/basic/container/VBoxExample.java
+++ JavaFXProject/src/basic/container/VBoxExample.java
@@ -1,6 +1,8 @@
 package basic.container;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -22,7 +24,7 @@ public class VBoxExample extends Application{
 		ImageView iv = new ImageView();
 		iv.setFitWidth(200);
 		iv.setPreserveRatio(true);
-		iv.setImage(new Image("/basic/images/javafx.jpg"));
+		iv.setImage(new Image("/basic/images/fruit1.jpg"));
 		
 		
 		HBox hbox = new HBox();
@@ -32,12 +34,41 @@ public class VBoxExample extends Application{
 		Button btnPrev = new Button();
 		btnPrev.setText("이전");
 		Button btnNext = new Button("다음");
+		
+		
+		
 		HBox.setHgrow(btnNext, Priority.ALWAYS);
 		btnNext.setMaxWidth(Double.MAX_VALUE);
 		hbox.getChildren().add(btnPrev);
 		hbox.getChildren().add(btnNext);
 		
 		VBox.setMargin(hbox, new Insets(10));
+		
+		//이벤트 핸들러를 해당 컨트롤에 등록.
+		btnNext.setOnAction(new EventHandler<ActionEvent>() {
+			int loc =1;
+			@Override
+			public void handle(ActionEvent ae) {
+				if(loc== 9) {
+					loc=1;
+				}
+				iv.setImage(new Image("/basic/images/fruit"+loc++ +".jpg"));
+				
+			}//액션이 발생했을때 set이라는 메소드로 핸들러를 등록하겠다.
+		});
+		
+		btnPrev.setOnAction(new EventHandler<ActionEvent>() {
+			int loc =8;
+			@Override
+			public void handle(ActionEvent ad) {
+				if(loc== 9) {
+					loc=1;
+				}
+				iv.setImage(new Image("/basic/images/fruit"+loc-- +".jpg"));
+				
+			}
+			
+		});
 		
 		root.getChildren().add(iv);
 		root.getChildren().add(hbox);
